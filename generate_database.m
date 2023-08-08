fcff6ab18d6efaeada371ed83031c9896bdd0092
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2023-08-08 16:44:13 -0300

    correction database generation

--- generate_database.m
+++ generate_database.m
@@ -5,87 +5,86 @@ clear all;
 init_params;
 
 %% Simulation parameters
-step_time = 0.01; % Simulation step time
-sim_time = 10;   % Simulation time in seconds
+step_time = 0.1; % Simulation step time
+sim_time = 1000;   % Simulation time in seconds
 
 %% Generate time vector
 time = 0:step_time:sim_time;
 
-%% Tamanho do dataset
-M = 1;
+%% Tamanho do datase
+n_inputs = 2;
+n_outputs = 2;
 N = length(time);
-signals = round(rand(M, N));  % Generating random binary values
-outputs = zeros(M,2*N);
-inputs = zeros(M, 2*N);
+outputs = zeros(N, n_outputs);
+inputs = zeros(N, n_inputs);
 
-for m=1:M
-    %% Open Simulink model
-    model_name = 'gmaw_process';
-    open_system(model_name);
+%% Open Simulink model
+model_name = 'gmaw_process';
+open_system(model_name);
 
-    %% Setup Signal Builder block
-    upper_value = fo; % Upper value for the binary signal
-    lower_value = fo/2; % Lower value for the binary signal
-    frequency = 10;   % Frequency of the binary signal (Hz)
+%% Setup Signal Builder block
+upper_value = fo; % Upper value for the binary signal
+lower_value = fo/2; % Lower value for the binary signal
 
-    %% Generate a pseudo-random binary signal using the parameters
-    f_signal = lower_value + (upper_value - lower_value) * (rand(size(time)) > 0.5);
-    Ir_signal = ones(1, N)*Ir;
-    inputs(m,:) = [f_signal Ir_signal];
-  
-    %% Setup Signal Builder block in the model
-    signal_f_path = [model_name, '/Signal f']; % Update with your Signal Builder block's path
-    signal_f_block = get_param(signal_f_path, 'Handle');
-    % Set the generated binary signal using the 'set' function
-    signalbuilder(signal_f_block, 'set', 1, 1, time, f_signal);
+%% Generate a pseudo-random binary signal using the parameters
+f_signal = lower_value + (upper_value - lower_value) * (rand(size(time)) > 0.5);
+Ir_signal = ones(1, N)*Ir;
+inputs(:, 1) = f_signal';
+inputs(:,2) = Ir_signal';
 
-    %% Simulate
-    % Define simulation options structure
-    simOut = sim(model_name, 'FixedStep', num2str(step_time), 'StopTime', num2str(sim_time));
+%% Setup Signal Builder block in the model
+signal_f_path = [model_name, '/Signal f']; % Update with your Signal Builder block's path
+signal_f_block = get_param(signal_f_path, 'Handle');
+% Set the generated binary signal using the 'set' function
+signalbuilder(signal_f_block, 'set', 1, 1, time, f_signal);
 
-    tout = simOut.tout;
-    we = simOut.we.Data;
-    we = resample(we', tout, 1/step_time);
-    h = simOut.h.Data;
-    h = resample(h', tout, 1/step_time);
+%% Simulate
+% Define simulation options structure
+simOut = sim(model_name, 'FixedStep', num2str(step_time), 'StopTime', num2str(sim_time));
 
-    % Generate Gaussian noise
-    noise_mean = 0;      % Mean of the Gaussian noise
-    noise_stddev = 1e-6;  % Standard deviation of the Gaussian noise
-    gaussian_noise = noise_mean + noise_stddev * randn(size(time));
-    
-    we = we + gaussian_noise;
-    h = h + gaussian_noise;
-    
-    outputs(m,:) = [we h];
-    
-    %% Plot
-%     figure;
-%     subplot(2,1,1);
-%     plot(time, f_signal);
-%     xlabel('t');
-%     ylabel('f');
-%     title('Input f');
-%     subplot(2,1,2);
-%     plot(time, Ir_signal);
-%     xlabel('t');
-%     ylabel('Ir');
-%     title('Input Ir');
-%     sgtitle('Inputs', 'FontSize', 16); % Adjust font size if needed
+tout = simOut.tout;
+we = simOut.we.Data;
+we = resample(we', tout, 1/step_time);
+h = simOut.h.Data;
+h = resample(h', tout, 1/step_time);
 
-%     figure;
-%     subplot(2,1,1);
-%     plot(time, we);
-%     xlabel('t');
-%     ylabel('we');
-%     title('Output we');
-%     subplot(2,1,2);
-%     plot(time, h);
-%     xlabel('t');
-%     ylabel('h');
-%     title('Output h');
-%     sgtitle('Outputs', 'FontSize', 16); % Adjust font size if needed
-end
+% Generate Gaussian noise
+noise_mean = 0;      % Mean of the Gaussian noise
+noise_stddev = 1e-6;  % Standard deviation of the Gaussian noise
+gaussian_noise = noise_mean + noise_stddev * randn(size(time));
+
+we = we + gaussian_noise;
+h = h + gaussian_noise;
+
+outputs(:,1) = we;
+outputs(:,2) = h;
+
+%% Plot
+% figure;
+% subplot(2,1,1);
+% plot(time, f_signal);
+% xlabel('t');
+% ylabel('f');
+% title('Input f');
+% subplot(2,1,2);
+% plot(time, Ir_signal);
+% xlabel('t');
+% ylabel('Ir');
+% title('Input Ir');
+% sgtitle('Inputs', 'FontSize', 16); % Adjust font size if needed
+% 
+% figure;
+% subplot(2,1,1);
+% plot(time, we);
+% xlabel('t');
+% ylabel('we');
+% title('Output we');
+% subplot(2,1,2);
+% plot(time, h);
+% xlabel('t');
+% ylabel('h');
+% title('Output h');
+% sgtitle('Outputs', 'FontSize', 16); % Adjust font size if needed
 
 %% Save data
 csvwrite('database/inputs.csv',inputs)
