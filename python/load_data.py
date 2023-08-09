cd14c83c6571261245337b7afbfec40a1d6a0b6d
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2023-08-09 16:14:36 -0300

    corrections

--- python/load_data.py
+++ python/load_data.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.preprocessing import StandardScaler
 
 # Load data
-inputs = pd.read_csv('C:/Users/lucas/OneDrive/Documentos/GitHub/lstm-control-waam/database/inputs.csv')
-outputs = pd.read_csv('C:/Users/lucas/OneDrive/Documentos/GitHub/lstm-control-waam/database/outputs.csv')
+data_dir = 'C:/Users/lucas/OneDrive/Documentos/GitHub/lstm-control-waam/database/'
+X = pd.read_csv(data_dir + 'train_inputs.csv')
+Y = pd.read_csv(data_dir + 'train_outputs.csv')
 
 # Process database
+scaler = StandardScaler()
+for col in Y.columns:
+    arr = Y[col].to_numpy().reshape(-1, 1)
+    Y[col] = scaler.fit_transform(arr)
+
+# Export Data
+data = pd.DataFrame(columns=list(X.columns) + list(Y.columns))
+data[X.columns] = X
+data[Y.columns] = Y
+
+data.to_csv(data_dir + 'train_data.csv')
