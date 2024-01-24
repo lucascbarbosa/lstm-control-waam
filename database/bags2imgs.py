afc56b8fd87a15115827574a27f90917885e56cd
Lucas Barbosa <lucas.barbosa.0899@poli.ufrj.br>
2024-01-24 17:29:32 -0300

    video export

--- database/bags2imgs.py
+++ database/bags2imgs.py
@@ -5,21 +5,34 @@ import cv2
 
 # Bag paths
 bag_file_path = "database/experiment/bags/bead1.bag"
+video_file_path = "database/experiment/videos/bead1.avi"
 
 bridge = CvBridge()
 
 show = False
+
+list_images = []
 with rosbag.Bag(bag_file_path, 'r') as bag:
     # Iterate through the messages in the bag
     for topic, msg, t in bag.read_messages():
         # Process the messages as needed
         if topic == "/xiris/image_raw":
-            
+
             # Extract image
             image = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+            height, width = image.shape
+            size = (width, height)
+            list_images.append(image)
 
             # Show Image    
-            # if show:
-            #     cv2.imshow("Image", image)
-            #     cv2.waitKey(0)
-            #     cv2.destroyAllWindows()
+            if show:
+                cv2.imshow("Image", image)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+
+
+out = cv2.VideoWriter(video_file_path,cv2.VideoWriter_fourcc(*'XVID'), 30, size)
+ 
+for i in range(len(list_images)):
+    out.write(list_images[i])
+out.release()
